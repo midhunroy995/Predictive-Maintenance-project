03646e52660e32355933e4737c63e14838ec5ad3
Midhun Roy <midhunroy7510@gmail.com>
2024-08-08 09:02:45 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,51 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.1.0"
+
+REPO_NAME = "Predictive-Maintenance-project"
+AUTHOR_USER_NAME = "midhunroy995"
+SRC_REPO = "MachineFailurePrediction"
+AUTHOR_EMAIL = "midhunroy7510@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A Python package for a predictive maintenance project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scikit-learn",
+        "matplotlib",
+        "seaborn",
+        "joblib",
+        "pyyaml"
+    ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "black",
+            "flake8",
+            "mypy",
+            "jupyter"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.11.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",
+)
